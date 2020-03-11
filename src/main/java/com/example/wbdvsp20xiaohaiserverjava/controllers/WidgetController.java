f10a0bc456e5b402222b693d464b048c366078b6
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-10 23:49:30 -0700

    Initial commit.

---
+++ src/main/java/com/example/wbdvsp20xiaohaiserverjava/controllers/WidgetController.java
@@ -0,0 +1,77 @@
+package com.example.wbdvsp20xiaohaiserverjava.controllers;
+
+import com.example.wbdvsp20xiaohaiserverjava.models.Widget;
+import com.example.wbdvsp20xiaohaiserverjava.services.TopicService;
+import com.example.wbdvsp20xiaohaiserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+
+    @Autowired
+    WidgetService service;
+
+    @Autowired
+    TopicService topicService;
+
+    @GetMapping("/api/widgets/{wid}/update")
+    public int updateWidgetNotRestfulBad(
+            @PathVariable("wid") int widgetId) {
+        Widget widget = new Widget();
+        widget.setTitle("New and Improved Title");
+        widget.setSize(123);
+        return service.updateWidget(widgetId, widget);
+    }
+
+    @PutMapping("/api/widgets/{wid}")
+    public int updateWidget(@PathVariable("wid") int widgetId,
+                            @RequestBody Widget widget) {
+        return service.updateWidget(widgetId, widget);
+    }
+
+    @GetMapping("/api/widgets/{wid}/delete")
+    public int deleteWidgetNotRestful(@PathVariable("wid") int widgetId) {
+        return service.deleteWidget(widgetId);
+    }
+
+    @DeleteMapping("/api/widgets/{wid}")
+    public int deleteWidget(@PathVariable("wid") int widgetId) {
+        return service.deleteWidget(widgetId);
+    }
+
+    @GetMapping("/api/widgets/create")
+    public Widget createWidgetNotRest() {
+        Widget newWidget = new Widget();
+        newWidget.setTitle("Not RESTful");
+        newWidget.setSize(45);
+        return service.createWidget(newWidget);
+    }
+
+    @PostMapping("/api/topics/{topicId}/widgets")
+    public Widget createWidget(
+            @PathVariable("topicId") Integer topicId,
+            @RequestBody Widget newWidget) {
+        return topicService.createWidgetForTopic(topicId, newWidget);
+//        return service.createWidget(topicId, newWidget);
+    }
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/widgets/{widgetId}")
+    public Widget findWidgetById(@PathVariable("widgetId") int wid) {
+        return service.findWidgetById(wid);
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(@PathVariable("tid") int tid) {
+        return service.findWidgetsForTopic(tid);
+    }
+
+}
---
+++ src/main/webapp/user-admin/user-admin.js
@@ -0,0 +1,99 @@
+// IIFE
+// Immediately Invoked Function Expressions
+(function () {
+    let userService = new AdminUserServiceClient()
+
+    let h1 = $("h1")
+    h1.html("User Admin Screen!!!")
+        .css("color", "green")
+    // .remove()
+
+    let users = [];
+    let userList = $("#user-list")
+    let usernameFld = $("#username-fld")
+
+    const deleteUser = (index) => {
+
+        const userId = users[index]._id;
+        userService.deleteUser(userId)
+            .then(response => {
+                users.splice(index, 1)
+                renderUsers()
+                // findAllUsers()
+            })
+
+    }
+    let currentUserId = -1
+    const editUser = index => {
+        const userId = users[index]._id;
+        currentUserId = userId;
+        userService.findUserById(userId)
+            .then(user => {
+                console.log(user)
+                usernameFld.val(user.username)
+            })
+    }
+
+    const renderUsers = () => {
+        userList.empty()
+        for (let u = 0; u < users.length; u++) {
+            const li = $("<li>")
+            // li.html(users[u])
+            const editBtn = $("<button>Edit</button>")
+            editBtn.click(() => {
+                editUser(u)
+            })
+
+            const deleteBtn = $("<button>Delete</button>")
+            deleteBtn.click(() => {
+                deleteUser(u)
+            })
+            li.append(deleteBtn)
+            li.append(editBtn)
+            li.append(users[u].username)
+            userList.append(li)
+        }
+    }
+    renderUsers()
+
+    const updateUser = () => {
+        const username = usernameFld.val()
+        usernameFld.val("")
+
+        userService.updateUser(currentUserId, {username: username})
+            .then(newUser => {
+                // users.push(newUser)
+                // renderUsers()
+                findAllUsers()
+            })
+
+    }
+
+    const createBtn = $("#create-btn");
+    const updateBtn = $("#update-btn");
+
+    updateBtn.click(updateUser)
+
+    const createUser = () => {
+        const username = usernameFld.val()
+        usernameFld.val("")
+
+        userService.createUser({username: username})
+            .then(newUser => {
+                // users.push(newUser)
+                // renderUsers()
+                findAllUsers()
+            })
+    }
+
+    createBtn.click(createUser)
+
+    const findAllUsers = () =>
+        userService.findAllUsers()
+            .then((theUsers) => {
+                users = theUsers
+                renderUsers()``
+        })
+
+    findAllUsers()
+})();
