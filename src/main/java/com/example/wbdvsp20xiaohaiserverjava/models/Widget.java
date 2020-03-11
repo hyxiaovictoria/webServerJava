b9cf0a9974861c3bef3e14d55aa8421a517ec3bc
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-11 00:00:54 -0700

    Added more vairables to Widget model.

--- src/main/java/com/example/wbdvsp20xiaohaiserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp20xiaohaiserverjava/models/Widget.java
@@ -13,7 +13,13 @@ public class Widget {
     private String title = "New Widget";
     private String type = "HEADING";
     private String text = "New Widget";
+    private int order;
+    private String src, url, href;
     private int size = 2;
+    private int width, height;
+    private String cssClass;
+    private String style;
+    private String value;
 
     @ManyToOne
     @JsonIgnore
@@ -65,5 +71,77 @@ public class Widget {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
