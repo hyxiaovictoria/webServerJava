d81b1549c61d81473dda7257275b789bdf8f8fc7
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-11 00:17:19 -0700

    Added two methods to TopicRepository.

--- src/main/java/com/example/wbdvsp20xiaohaiserverjava/repositories/TopicRepository.java
+++ src/main/java/com/example/wbdvsp20xiaohaiserverjava/repositories/TopicRepository.java
@@ -1,6 +1,7 @@
 package com.example.wbdvsp20xiaohaiserverjava.repositories;
 
 import com.example.wbdvsp20xiaohaiserverjava.models.Topic;
+import com.example.wbdvsp20xiaohaiserverjava.models.Widget;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,13 @@ import java.util.List;
 
 public interface TopicRepository
         extends CrudRepository<Topic, Integer> {
+
+    @Query("SELECT topic FROM Topic topic WHERE topic.id=:topicId")
+    public Topic findTopicById(
+            @Param("topicId") int tid);
+
+    @Query("SELECT topic FROM Topic topic")
+    public List<Topic> findAllTopics();
 
     @Query(value = "SELECT * FROM topics WHERE topics.lesson_id=:lessonId", nativeQuery = true)
     public List<Topic> findTopicsForLesson(@Param("lessonId") String lessonId);
